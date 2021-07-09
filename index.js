59c8cbfe111d92961c227e52d22acd4139f3e953
gurrrrrrett3 <61361654+gurrrrrrett3@users.noreply.github.com>
2021-07-09 02:59:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'docs')));
   
 app.post('/', (req, res) => {
-    const hex = req.headers.hex;
+    const { hex }= req.body.json()
     setLEDcolor(hex) 
     res.sendStatus(200)
 })
